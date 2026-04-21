f666b2b5dc7c952cc26a80439aaf0ffd064e2033
Ginocarlo Velasquez <ginocarlovelasquez@gmail.com>
2026-04-21 10:16:00 +0800

    no message

--- src/modules/accountModule/authController.js
+++
@@ -1,9 +0,0 @@
-import * as userModel from "../models/userModel.js";
-
-export function signup(req) {
-  return userModel.createUser(req);
-}
-
-export function login(req) {
-  return userModel.loginUser(req.username, req.password);
-}
