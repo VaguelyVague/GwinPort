e2042fdbd07dd7eac28e64106fdd3be668346fca
Ginocarlo Velasquez <ginocarlovelasquez@gmail.com>
2026-04-27 22:39:38 +0800

    deleted old files

--- src/modules/Booking&Seat Module/controller/NotificationService.java
+++
@@ -1,9 +0,0 @@
-
-package controller;
-
-public class NotificationService {
-
-    public void sendConfirmation(String user, String ref){
-        System.out.println("Notification sent to " + user + " for booking " + ref);
-    }
-}
--- src/modules/Booking&Seat Module/controller/TicketService.java
+++
@@ -1,9 +0,0 @@
-
-package controller;
-
-public class TicketService {
-
-    public String generateTicket(String ref){
-        return "ETICKET-" + ref;
-    }
-}
